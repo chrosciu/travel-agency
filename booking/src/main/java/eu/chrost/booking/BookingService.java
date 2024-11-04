71640e776962de4c685bf06d5e2db7e7bb44f7d4
Marcin Chrost <mchrost82@gmail.com>
2024-11-04 22:31:31 +0100

    Parallelization with ExecutorService

--- booking/src/main/java/eu/chrost/booking/BookingService.java
+++ booking/src/main/java/eu/chrost/booking/BookingService.java
@@ -4,6 +4,12 @@ import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ThreadFactory;
+
 @Service
 @Slf4j
 public class BookingService {
@@ -12,10 +18,20 @@ public class BookingService {
         BACK
     }
 
+    /*
+     Default ThreadFactory used by StructuredTaskScope does not set names for created threads
+     which makes threads logging unusable
+    */
+    private final ThreadFactory threadFactory = Thread.ofVirtual().name("booking-", 0).factory();
+    private final ExecutorService executorService = Executors.newCachedThreadPool(threadFactory);
+
+    @SneakyThrows
     public String book(String destination) {
+        Future<String> there = executorService.submit(() -> book(destination, TripType.THERE));
+        Future<String> back = executorService.submit(() -> book(destination, TripType.BACK));
         return String.join("\n",
-            book(destination, TripType.THERE),
-            book(destination, TripType.BACK)
+            there.get(),
+            back.get()
         );
     }
 
