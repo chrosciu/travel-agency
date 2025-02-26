fecd7dc7ba7b13682d6ff94262321e48ee5ea874
Marcin Chrost <mchrost82@gmail.com>
2025-02-26 21:14:35 +0100

    Use blocking functions in coroutines with dispatcher based on virtual threads

--- kotlin-booking/src/main/kotlin/eu/chrost/kotlinbooking/KotlinBookingService.kt
+++ kotlin-booking/src/main/kotlin/eu/chrost/kotlinbooking/KotlinBookingService.kt
@@ -1,13 +1,24 @@
 package eu.chrost.kotlinbooking
 
 import io.github.oshai.kotlinlogging.KotlinLogging
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.delay
+
 import org.springframework.stereotype.Service
+import java.util.concurrent.Executors
+import kotlin.coroutines.CoroutineContext
 
 private val logger = KotlinLogging.logger {}
+
+object VirtualThreadDispatcher : CoroutineDispatcher() {
+    private val executor = Executors.newVirtualThreadPerTaskExecutor()
+
+    override fun dispatch(context: CoroutineContext, block: Runnable) {
+        executor.execute(block)
+    }
+}
 
 @Service
 class KotlinBookingService {
@@ -18,14 +29,14 @@ class KotlinBookingService {
 
     suspend fun book(destination: String): String = coroutineScope {
         listOf(
-            async { book(destination, TripType.THERE) },
-            async { book(destination, TripType.BACK) }
+            async(VirtualThreadDispatcher) { book(destination, TripType.THERE) },
+            async(VirtualThreadDispatcher) { book(destination, TripType.BACK) }
         ).awaitAll().joinToString("\n")
     }
 
     private suspend fun book(destination: String, tripType: TripType): String {
         logger.info { "[$destination $tripType] Booking start" }
-        delay(3000)
+        Thread.sleep(3000)
         logger.info { "[$destination $tripType] Booking end" }
         return "Booked $tripType travel to:  $destination"
     }
